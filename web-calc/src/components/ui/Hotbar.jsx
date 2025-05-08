b07cb46a2623e9509024b0b5ba3bf8d03b5a5459
luivrz <lluivrz@gmail.com>
2025-05-08 16:21:27 -0300

    centered hotbar

--- web-calc/src/components/ui/Hotbar.jsx
+++ web-calc/src/components/ui/Hotbar.jsx
@@ -6,16 +6,18 @@ function Hotbar() {
 
   return (
     <>
-    <div className="flex items-center justify-center gap-2 p-4 bg-gray-800 shadow-md my-2">
+    <div className="flex items-center justify-center p-4 bg-white-800 ">
+        <div className='bg-gray-800 p-4 rounded shadow-md flex space-x-4'>
       <button className="px-4 py-2 text-white bg-gray-800 rounded shadow-sm hover:bg-gray-700 hover:scale-110 hover:shadow-md transition-all duration-350 tracking-wider">Home</button>
       <button className="px-4 py-2 text-white bg-gray-800 rounded shadow-sm hover:bg-gray-700 hover:scale-110 hover:shadow-md transition-all duration-350 tracking-wider">About</button>
       <button className="px-4 py-2 text-white bg-gray-800 rounded shadow-sm hover:bg-gray-700 hover:scale-110 hover:shadow-md transition-all duration-350 tracking-wider">Services</button>
       <button 
         className="px-4 py-2 text-white bg-gray-800 rounded shadow-sm hover:bg-gray-700 hover:scale-110 hover:shadow-md transition-all duration-350 tracking-wider"
         onClick={() => setShowCalculator(!showCalculator)}
-      >
+        >
         Calculator
       </button>
+          </div>
     </div>      
         {showCalculator && <Calculator onClose={() => setShowCalculator(false)} />}   
     </>
