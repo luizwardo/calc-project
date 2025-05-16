9810f7e031c09aa326496c1a5fbb65a34bb4387a
luivrz <lluivrz@gmail.com>
2025-05-16 17:30:09 -0300

    ghpages

--- web-calc/vite.config.js
+++ web-calc/vite.config.js
@@ -5,11 +5,11 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/calc-project",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-  base: "calc-project"
     },
   },
 })
